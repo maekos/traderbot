bea27dc38bc74b3450f3965184313a4c2918887f
Marcos <maekos@gmail.com>
2020-10-04 17:53:47 -0300

    feat: grafica de correlación disponible

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM        python:3.7-slim-stretch as trader
 
+RUN apt-get update && apt-get install -y python3-tk #libx11-dev
+
 ENV         PYTHONUNBUFFERED 1
 ENV         PYTHONDONTWRITEBYTECODE 1
 
--- utils/utils.py
+++ utils/utils.py
@@ -17,3 +17,40 @@ def leer_excel(filename):
     data = data.sort_values('timestamp', ascending=True)
     data.set_index('timestamp', inplace=True)
     return data
+
+def ajustar_cierre(df):
+    df['factor'] = df.adjusted_close / df.close
+    df['VolMlnUSD'] = df.close * df.volume / 1000000
+    cols = [df.open * df.factor,
+            df.high * df.factor,
+            df.low * df.factor,
+            df.adjusted_close,
+            df.VolMlnUSD]
+    dataAj = pd.concat(cols, axis=1)
+    dataAj.columns = ['Open', 'High', 'Low', 'Close', 'VolMlnUSD']
+    return dataAj.round(2)
+
+def graficar_correlacion(df, title=''):
+    fig = plt.figure(figsize=(12, 8))
+    plt.matshow(df, fignum=fig.number, cmap='binary')
+    plt.xticks(range(df.shape[1]), df.columns, fontsize=12, rotation=90)
+    plt.yticks(range(df.shape[1]), df.columns, fontsize=12)
+
+    cb = plt.colorbar(orientation='vertical', label="Factor de correlación 'r'")
+    cb.ax.tick_params(labelsize=12)
+    plt.title(title, fontsize=16, y=1.15)
+
+    ax = plt.gca()
+
+    ax.set_xticks(np.arange(-.5, len(df), 1), minor=True);
+    ax.set_yticks(np.arange(-.5, len(df), 1), minor=True);
+    ax.grid(which='minor', color='w', linestyle='-', linewidth=3)
+
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            if df.iloc[i,j] > 0.6:
+                color = 'white'
+            else:
+                color = 'black'
+            fig.gca().text(i, j, "{:.2f}".format(df.iloc[i,j]), ha='center', va='center', c = color, size=14)
+    return plt
